39af8b227dc10d2a7266e7a4251005fa6ef8f587
İREM İÇYER <62353089+iremicyer@users.noreply.github.com>
2020-08-14 14:30:55 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+
+namespace Faktöriyel_Hesaplama
+{
+    class Program
+    {
+        public static int Faktöriyel(int sayi) // Faktöriyel Fonksiyonunun oluşturulması(özyinelemeli)
+        {
+            if (sayi == 0)
+            {
+                return 1;
+            }
+            else
+            {
+              ///  Console.WriteLine(sayi + " ! faktöriyel:" + sayi * Faktöriyel(sayi - 1));
+
+                return sayi * Faktöriyel(sayi - 1);
+            }
+        }
+        static void Main(string[] args)
+        {
+          
+            Console.Write("Bir sayi giriniz:");   // klavyeden faktöriyeli hesaplanacak sayının alınması
+
+            int s = Convert.ToInt32(Console.ReadLine()); // alınan sayının bir s değişkenine atanması.
+
+            Console.WriteLine(s + " ! faktöriyel:" + Faktöriyel(s));
+           
+            Console.ReadKey();
+
+        }
+    }
+}
